8399a5c3382004e679cd7a4a186fedd5b73beadc
Sebastian Triviño <justonly678@gmail.com>
2024-12-09 21:58:37 -0500

    creted the other urls to renderize components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { FoundationInscriptionComponent } from './foundation-inscription/foundat
 import { FoundationInscription2Component } from './foundation-inscription2/foundation-inscription2.component';
 import { FoundationInscription3Component } from './foundation-inscription3/foundation-inscription3.component';
 import { LoginComponent } from './auth/login/login.component';
+import { Step1Component } from './animal-report/components/step1/step1.component';
+import { Step2Component } from './animal-report/components/step2/step2.component';
+import { Step3Component } from './animal-report/components/step3/step3.component';
+import { FoundationsAdopetComponent } from './users/dashboard/foundations-adopet/foundations-adopet.component';
+import { AnimalsReportsComponent } from './FoundationDashboard/animals-reports/animals-reports.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -21,6 +26,32 @@ export const routes: Routes = [
   {
     path: 'form-signup-foundation3',
     component: FoundationInscription3Component,
+  },
+  {
+    path: 'form-report',
+    component: AnimalReportComponent,
+    children: [
+      {
+        path: 'stepone',
+        component: Step1Component,
+      },
+      {
+        path: 'steptwo',
+        component: Step2Component,
+      },
+      {
+        path: 'stepthree',
+        component: Step3Component,
+      },
+    ],
+  },
+  {
+    path: 'users-main',
+    component: FoundationsAdopetComponent,
+  },
+  {
+    path: 'foundations-main',
+    component: AnimalsReportsComponent,
   },
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' },
